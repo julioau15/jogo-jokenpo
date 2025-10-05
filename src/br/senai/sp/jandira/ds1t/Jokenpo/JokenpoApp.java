c02ba6a497abe8ab39a7f5e0408fddc13140753a
Julio Augusto <julioaugustov@gmail.com>
2025-10-05 16:18:32 -0300

    Adicionado a primeira saída do jogo

--- src/br/senai/sp/jandira/ds1t/Jokenpo/JokenpoApp.java
+++ src/br/senai/sp/jandira/ds1t/Jokenpo/JokenpoApp.java
@@ -4,6 +4,18 @@ import br.senai.sp.jandira.ds1t.Jokenpo.model.Jokenpo;
 
 public class JokenpoApp {
     public static void main(String[] args) {
+
+        System.out.println("****************************************");
+        System.out.println("**************  JOKÊNPO  ***************");
+        System.out.println("****************************************\n");
+        System.out.println("          >>> ESCOLHA UMA OPÇÃO <<<\n");
+        System.out.println("----------------------------------------");
+        System.out.println("|  1 - PEDRA                          |");
+        System.out.println("|  2 - PAPEL                          |");
+        System.out.println("|  3 - TESOURA                        |");
+        System.out.println("----------------------------------------");
+        System.out.println("****************************************");
+
         Jokenpo jokenpo = new Jokenpo();
         jokenpo.receberDados();
     }
